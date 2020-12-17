4f8fa6cc5ad044210459b1143a2cd67f15735236
Michael Predko <myke.predko@mimetics.ca>
2020-12-17 13:50:02 -0500

    Create Read_File.c

---
+++ Read_File.c
@@ -0,0 +1,78 @@
+/*
+ ============================================================================
+ Name        : Advent_2020_Day_Read_File.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Advent of Code 2020 in C, Ansi-style
+ ============================================================================
+ */
+
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(void) {
+
+FILE*    filePtr;
+int      fileSize;
+char*    fileData;
+int      lineNumber;
+int      freadSize;
+uint64_t answerValue = 0;
+int      i;
+
+
+#define doTest
+#if defined(doTest)
+  if (NULL == (filePtr = fopen("testInput.txt"
+                             , "r"))) {
+#else
+  if (NULL == (filePtr = fopen("actualInput.txt"
+                             , "r"))) {
+#endif
+    puts("\nFile Open Error");
+    return EXIT_FAILURE;
+  }
+
+  fseek(filePtr
+      , 0
+      , SEEK_END);
+  fileSize = ftell(filePtr);
+  fseek(filePtr
+      , 0
+      , SEEK_SET);
+
+  fileData = malloc(fileSize + 3);
+
+  freadSize = fread(fileData
+                  , sizeof(char)
+                  , fileSize
+                  , filePtr);
+  if ('\n' != fileData[freadSize - 1]) {
+    fileData[freadSize++] = '\n';
+  }
+  fileData[freadSize] = '\0';
+
+  for (i = lineNumber = 0; '\0' != fileData[i];) {
+    for (; '\n' != fileData[i]; ++i) {  }
+    ++lineNumber;  ++i;
+  }
+
+
+
+//todo: Put in Data Process code
+
+
+
+  free(fileData);
+  fclose(filePtr);
+
+  printf("\n%I64d"
+       , answerValue);
+
+	return EXIT_SUCCESS;
+
+}
